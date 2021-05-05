596db8f928049d0b6f99a519780e115428ed2d49
Furkan AYDIN <f.aydin@outlook.com.tr>
2021-05-06 02:09:53 +0300

    MainActivity file fixed for Android bug.

--- android/app/src/main/java/com/moviem/MainActivity.java
+++ android/app/src/main/java/com/moviem/MainActivity.java
@@ -1,6 +1,14 @@
 package com.moviem;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+
+// https://docs.swmansion.com/react-native-gesture-handler/docs/
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+// https://github.com/zoontek/react-native-bootsplash
+import com.zoontek.rnbootsplash.RNBootSplash;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +19,21 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "Moviem";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    RNBootSplash.init(R.drawable.bootsplash, MainActivity.this);
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()){
+        @Override
+        protected ReactRootView createRootView() {
+            return new RNGestureHandlerEnabledRootView(MainActivity.this);
+        }
+    }
   }
 }
